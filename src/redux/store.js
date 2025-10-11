447dfa4a1f0c9b2c008444df57cf053329e9c79f
bharatroy4D <bharatroy1005@gmail.com>
2025-10-11 16:40:00 +0600

    ProductSlice is store into added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { baseApi } from "./baseApi/baseApi"
 import authReducer from "./feature/auth/authSlice"
+import productReducer from "./feature/Products/ProductsSlice"
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
         auth: authReducer,
+        product: productReducer,
     },
     middleware: (getDefaultMiddleWare) =>
         getDefaultMiddleWare().concat(baseApi.middleware)
